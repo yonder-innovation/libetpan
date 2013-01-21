6c9ec270fbd319003c5e5d7e63f10592077ad243
Matt Ronge <mronge@mronge.com>
2013-01-21 15:20:31 -0600

    Turn off use of mmap strings. This way iOS devices can open files > 8MB

--- build-mac/include/config.h
+++ build-mac/include/config.h
@@ -170,6 +170,8 @@
 /* Version number of package */
 #define VERSION "1.1"
 
+#define MMAP_UNAVAILABLE
+
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
